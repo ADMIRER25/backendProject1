4a52f10493dea0afe6b0e5a3bb008a83c49311ba
Subha Nayak <subhanayak022@gmail.com>
2024-04-22 22:46:32 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,73 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+const registerUser = asyncHandler(async (req, res) => {
+  //json response
+  // res.status(200).json({
+  //     message:"ok"
+  // })
 
-const registerUser = asyncHandler(async(req,res)=>{
-    //json response
-    res.status(200).json({
-        message:"ok"
+  //1.get user details from frontend
+  const { fullName, email, userName, password } = req.body;
+  console.log("email: ", email);
+
+  //2.validation - not empty and other check
+//   if (fullName === "") {
+//     throw new ApiError(400, "Full name is required");
+//   }
+    if(
+        [fullName,email,userName,password].some((field)=>field?.trim() === "")
+    ){
+        throw new ApiError(400,"All fields are required");
+    }
+  //3.check if user already exists: username,email
+    const existedUser = User.findOne({
+        $or:[{userName},{email}]
     })
-})
+    if(existedUser){
+        throw new ApiError(409,"User already exists");
+    }
+  //3.chcek for images and avatar
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    console.log("avatarLocalPath: ", avatarLocalPath);
+    const coverImageLocalPath = req.files?.coverImage[0].path
 
-export {registerUser}
+    if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar file is required");
+    }
+
+  //4.upload to cloudinary
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+   //5.as avatar is required so again check for avatar uploaded in cloudinary on not
+    if(!avatar) {
+        throw new ApiError(500,"Error in uploading avatar");
+    }
+
+  //6.create user object
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    userName :userName.toLowerCase()
+  })
+  //create user object - create entry in db
+  //7.chcek for user creation and remove password and refresh token field from response
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+  if(!createdUser){
+    throw new ApiError(500,"Something went wrong while registering the user");
+  }
+  //return response
+  return res.status(201).json(
+    new ApiResponse(200,createdUser,"User Registered Successfully")
+  )
+});
+
+export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewires/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    //middleware
+    upload.fields([
+        {
+            //should be same name field in frontend
+            name : "avatar",
+            maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount : 1
+        }
+    ]),
+    //controller
+    registerUser)
 
 export default router;
